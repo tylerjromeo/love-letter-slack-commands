ccd4f7e1b05671e4fdb631523db346f165f1998c
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-02-15 09:37:32 -0500

    refactor game to user state monads

--- src/main/scala-2.11/org/romeo/loveletter/game/Engine.scala
+++ src/main/scala-2.11/org/romeo/loveletter/game/Engine.scala
@@ -1,5 +1,7 @@
 package org.romeo.loveletter.game
 
+import scalaz.State
+
 case class Game(val players: Seq[Player]) {
   require(players.length >= 2, "Need at least 2 players")
   require(players.length <= 4, "No more than 4 players")
@@ -7,9 +9,10 @@ case class Game(val players: Seq[Player]) {
 
 object Game {
   def apply(playerNames: => Seq[String]) = new Game(playerNames.map(Player(_)))
-  def currentPlayer(g: Game): Player = g.players.head
-  def endTurn(g: Game): Game = {
-    Game(g.players.tail ++ Seq(g.players.head))
+  def currentPlayer = State[Game, Player]{g: Game => (g, g.players.head)}
+  //Cycles the list of players so the player in front is last. Returns the new current player
+  def endTurn = State[Game, Player] {
+    g: Game => (Game(g.players.tail ++ Seq(g.players.head)), g.players(1))
   }
 }
 
--- src/test/scala-2.11/org/romeo/loveletter/game/EngineTest.scala
+++ src/test/scala-2.11/org/romeo/loveletter/game/EngineTest.scala
@@ -14,20 +14,28 @@ class EngineSpec extends FlatSpec with Matchers {
   "A game" should "Have the first player as the current player" in {
     val players = Seq("Tyler", "Kevin", "Morgan", "Trevor")
     val game = Game(players);
-    Game.currentPlayer(game).name should be (players.head)
+    Game.currentPlayer.eval(game).name should be (players.head)
   }
 
   "A game's first player" should "change when a turn is ended" in {
     val players = Seq("Tyler", "Kevin", "Morgan", "Trevor")
     var game = Game(players);
-    Game.currentPlayer(game).name should be (players.head)
-    game = Game.endTurn(game)
-    Game.currentPlayer(game).name should be (players(1))
-    game = Game.endTurn(game)
-    Game.currentPlayer(game).name should be (players(2))
-    game = Game.endTurn(game)
-    Game.currentPlayer(game).name should be (players(3))
-    game = Game.endTurn(game)
-    Game.currentPlayer(game).name should be (players.head)
+    Game.endTurn.exec(game).players.head.name should be (players(1))
+  }
+
+  "A game" should "be back to its initial state after a full round of turns" in {
+    val players = Seq("Tyler", "Kevin", "Morgan", "Trevor")
+    var game = Game(players);
+    def FullRoundCycle() = for {
+      p1 <- Game.currentPlayer
+      p2 <- Game.endTurn
+      p3 <- Game.endTurn
+      p4 <- Game.endTurn
+      _ <- Game.endTurn
+    } yield Seq(p1, p2, p3, p4)
+    //after 4 turns change the state should be back to where it started
+    val (newGame, newPlayers) = FullRoundCycle()(game)
+    newGame should be (game)
+    newPlayers.map(_.name) should be (players)
   }
 }

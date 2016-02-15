2f6903d12c82c79e4efff0897ef2253872914b69
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-02-14 20:45:33 -0500

    add slack form fields to routes

--- src/main/scala-2.11/org/romeo/loveletter/http/Main.scala
+++ src/main/scala-2.11/org/romeo/loveletter/http/Main.scala
@@ -1,10 +1,10 @@
 package org.romeo.loveletter.http
 /**
-  * User: tylerromeo
-  * Date: 2/8/16
-  * Time: 2:20 PM
-  *
-  */
+ * User: tylerromeo
+ * Date: 2/8/16
+ * Time: 2:20 PM
+ *
+ */
 
 import akka.actor.ActorSystem
 import spray.http.{MediaTypes, StatusCodes}
@@ -19,10 +19,13 @@ object Main extends App with SimpleRoutingApp {
   implicit val system = ActorSystem("my-system")
 
   startServer(interface = "localhost", port = Properties.envOrElse("PORT", "8080").toInt) {
-    path(Rest) { id =>
-      get {
-        respondWithMediaType(MediaTypes.`application/json`) {
-          complete("{\"item\": \"" + id + "\"}")
+    pathSingleSlash {
+      post {
+        formFields('token, 'team_id, 'team_domain, 'channel_id, 'channel_name, 'user_id, 'user_name, 'command, 'text, 'response_url) {
+          (token, teamId, teamDomain, channelId, channelName, userId, userName, command, test, responseUrl) =>
+            respondWithMediaType(MediaTypes.`application/json`) {
+              complete("{\"item\": \"test\"}") //TODO: connect routes to game
+            }
         }
       }
     }

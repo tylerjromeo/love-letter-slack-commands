bc023bdceb4bb8dc86dda7326297a7d2aa5dac83
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-02-15 09:37:19 -0500

    refactor deck to use case objects instead of classes

--- src/main/scala-2.11/org/romeo/loveletter/game/Deck.scala
+++ src/main/scala-2.11/org/romeo/loveletter/game/Deck.scala
@@ -1,14 +1,14 @@
 package org.romeo.loveletter.game
 
 object Deck {
-  val cards = Seq.fill(5)(new Guard) ++
-    Seq.fill(2)(new Priest) ++
-    Seq.fill(2)(new Baron) ++
-    Seq.fill(2)(new Handmaid) ++
-    Seq.fill(2)(new Prince) ++
-    Seq.fill(1)(new King) ++
-    Seq.fill(1)(new Countess) ++
-    Seq.fill(1)(new Princess)
+  val cards = Seq.fill(5)(Guard) ++
+    Seq.fill(2)(Priest) ++
+    Seq.fill(2)(Baron) ++
+    Seq.fill(2)(Handmaid) ++
+    Seq.fill(2)(Prince) ++
+    Seq.fill(1)(King) ++
+    Seq.fill(1)(Countess) ++
+    Seq.fill(1)(Princess)
 }
 
 trait Card {
@@ -18,42 +18,42 @@ trait Card {
   def doAction(state: Game): Unit = {}
 }
 
-class Guard extends Card {
+case object Guard extends Card {
   val value = 1
   val description = "Name a non-Guard card and choose another player. If that player has that card, he or she is out of the round."
 }
 
-class Priest extends Card {
+case object Priest extends Card {
   val value = 2
   val description = "Look at another player's hand."
 }
 
-class Baron extends Card {
+case object Baron extends Card {
   val value = 3
   val description = "You and another player secretly compare hands. The player with the lower value is out of the round."
 }
 
-class Handmaid extends Card {
+case object Handmaid extends Card {
   val value = 4
   val description = "Until your next turn, ignore all effects from other player's cards."
 }
 
-class Prince extends Card {
+case object Prince extends Card {
   val value = 5
   val description = "Choose any player (inluding yourself) ato discard his or her hand and draw a new card."
 }
 
-class King extends Card {
+case object King extends Card {
   val value = 6
   val description = "Trade hands with another player of your choice."
 }
 
-class Countess extends Card {
+case object Countess extends Card {
   val value = 7
   val description = "If you have this card and the King or Prince in your hand, you must discard this card."
 }
 
-class Princess extends Card {
+case object Princess extends Card {
   val value = 8
   val description = "If you discard this card, you are out of the round."
 }

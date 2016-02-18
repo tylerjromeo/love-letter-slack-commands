2c400fc52c97451ed7b3e1300b1a65c841cf21bb
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-02-18 11:21:55 -0500

    add values to card trait for if target and guess are required for their action

--- src/main/scala-2.11/org/romeo/loveletter/game/Deck.scala
+++ src/main/scala-2.11/org/romeo/loveletter/game/Deck.scala
@@ -16,6 +16,8 @@ object Deck {
 trait Card {
   val value: Int
   val description: String
+  val requiresTarget: Boolean
+  val requiresGuess: Boolean
   def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card]): State[Game, Player] = ???
 }
 
@@ -27,47 +29,63 @@ object Card {
 case object Guard extends Card {
   val value = 1
   val description = "Name a non-Guard card and choose another player. If that player has that card, he or she is out of the round."
+  val requiresTarget: Boolean = true
+  val requiresGuess: Boolean = true
   override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card]): State[Game, Player] = ???
 }
 
 case object Priest extends Card {
   val value = 2
   val description = "Look at another player's hand."
+  val requiresTarget: Boolean = true
+  val requiresGuess: Boolean = false
   override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Baron extends Card {
   val value = 3
   val description = "You and another player secretly compare hands. The player with the lower value is out of the round."
+  val requiresTarget: Boolean = true
+  val requiresGuess: Boolean = false
   override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Handmaid extends Card {
   val value = 4
   val description = "Until your next turn, ignore all effects from other player's cards."
+  val requiresTarget: Boolean = false
+  val requiresGuess: Boolean = false
   override def doAction(card: Card, discarder: Player, target: Option[Player] = None, guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Prince extends Card {
   val value = 5
   val description = "Choose any player (inluding yourself) ato discard his or her hand and draw a new card."
+  val requiresTarget: Boolean = true
+  val requiresGuess: Boolean = false
   override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object King extends Card {
   val value = 6
   val description = "Trade hands with another player of your choice."
+  val requiresTarget: Boolean = true
+  val requiresGuess: Boolean = false
   override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Countess extends Card {
   val value = 7
   val description = "If you have this card and the King or Prince in your hand, you must discard this card."
+  val requiresTarget: Boolean = false
+  val requiresGuess: Boolean = false
   override def doAction(card: Card, discarder: Player, target: Option[Player] = None, guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Princess extends Card {
   val value = 8
   val description = "If you discard this card, you are out of the round."
+  val requiresTarget: Boolean = false
+  val requiresGuess: Boolean = false
   override def doAction(card: Card, discarder: Player, target: Option[Player] = None, guess: Option[Card] = None): State[Game, Player] = ???
 }

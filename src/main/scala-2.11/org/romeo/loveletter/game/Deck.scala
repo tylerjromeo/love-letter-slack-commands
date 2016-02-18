1285652ba9c937725c74a720dddbe65c54590134
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-02-17 19:59:56 -0500

    Add stubs for card action methods

--- src/main/scala-2.11/org/romeo/loveletter/game/Deck.scala
+++ src/main/scala-2.11/org/romeo/loveletter/game/Deck.scala
@@ -1,5 +1,7 @@
 package org.romeo.loveletter.game
 
+import scalaz.State
+
 object Deck {
   val cards = Seq.fill(5)(Guard) ++
     Seq.fill(2)(Priest) ++
@@ -14,11 +16,10 @@ object Deck {
 trait Card {
   val value: Int
   val description: String
-  //TODO: this interface needs to be able to deal with player inputs too... figure that out
-  def doAction(state: Game): Unit = {}
+  def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card]): State[Game, Player] = ???
 }
 
-object Card {  
+object Card {
   implicit object CardOrdering extends Ordering[Card] {
     def compare(a:Card, b:Card) = a.value compare b.value
   }
@@ -27,39 +28,47 @@ object Card {
 case object Guard extends Card {
   val value = 1
   val description = "Name a non-Guard card and choose another player. If that player has that card, he or she is out of the round."
+  override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card]): State[Game, Player] = ???
 }
 
 case object Priest extends Card {
   val value = 2
   val description = "Look at another player's hand."
+  override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Baron extends Card {
   val value = 3
   val description = "You and another player secretly compare hands. The player with the lower value is out of the round."
+  override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Handmaid extends Card {
   val value = 4
   val description = "Until your next turn, ignore all effects from other player's cards."
+  override def doAction(card: Card, discarder: Player, target: Option[Player] = None, guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Prince extends Card {
   val value = 5
   val description = "Choose any player (inluding yourself) ato discard his or her hand and draw a new card."
+  override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object King extends Card {
   val value = 6
   val description = "Trade hands with another player of your choice."
+  override def doAction(card: Card, discarder: Player, target: Option[Player], guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Countess extends Card {
   val value = 7
   val description = "If you have this card and the King or Prince in your hand, you must discard this card."
+  override def doAction(card: Card, discarder: Player, target: Option[Player] = None, guess: Option[Card] = None): State[Game, Player] = ???
 }
 
 case object Princess extends Card {
   val value = 8
   val description = "If you discard this card, you are out of the round."
+  override def doAction(card: Card, discarder: Player, target: Option[Player] = None, guess: Option[Card] = None): State[Game, Player] = ???
 }

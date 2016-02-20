274b0a462784169ed619007ddf51d2b463f88a59
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-02-20 13:35:17 -0500

    add json type for slack message response

--- src/main/scala-2.11/org/romeo/loveletter/http/Main.scala
+++ src/main/scala-2.11/org/romeo/loveletter/http/Main.scala
@@ -10,13 +10,36 @@ import akka.actor.ActorSystem
 import spray.http.{MediaTypes, StatusCodes}
 import spray.json._
 import spray.routing.SimpleRoutingApp
-
 import scala.util.{Properties, Success, Failure, Try}
-
 import org.romeo.loveletter.persistence.MemoryDataStore
+import spray.httpx.SprayJsonSupport
+import spray.json.RootJsonFormat
+
+case class SlackResponse(privateMessage: Boolean, text: String)
+
+object SlackResponseJsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
+  implicit object SlackResponseJsonFormats extends RootJsonFormat[SlackResponse] {
+    def write(r: SlackResponse) = {
+      if(r.privateMessage) {
+        JsObject(Map("response_type" -> JsString("in_channel"), "text" -> JsString(r.text)))
+      } else {
+        JsObject(Map("text" -> JsString(r.text)))
+      }
+    }
+
+    def read(value: JsValue) = {
+      value.asJsObject.getFields("response_type", "text") match {
+        case Seq(JsString(response_type), JsString(text)) => new SlackResponse(true, text)
+        case Seq(JsString(text)) => new SlackResponse(false, text)
+        case _ => throw new DeserializationException("SlackResponse expected")
+      }
+    }
+  }
+}
 
 object Main extends App with SimpleRoutingApp {
   implicit val system = ActorSystem("my-system")
+  import SlackResponseJsonSupport._
 
   startServer(interface = "localhost", port = Properties.envOrElse("PORT", "8080").toInt) {
     pathSingleSlash {
@@ -24,7 +47,7 @@ object Main extends App with SimpleRoutingApp {
         formFields('token, 'team_id, 'team_domain, 'channel_id, 'channel_name, 'user_id, 'user_name, 'command, 'text, 'response_url) {
           (token, teamId, teamDomain, channelId, channelName, userId, userName, command, test, responseUrl) =>
             respondWithMediaType(MediaTypes.`application/json`) {
-              complete("{\"item\": \"test\"}") //TODO: connect routes to game
+              complete(SlackResponse(false, "test hello")) //TODO: connect routes to game
             }
         }
       }

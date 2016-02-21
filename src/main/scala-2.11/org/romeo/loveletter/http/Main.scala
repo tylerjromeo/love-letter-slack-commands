54169cec01e2d49ea7c433bc642d9510ecba4be0
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-02-20 19:58:54 -0500

    add help command

--- src/main/scala-2.11/org/romeo/loveletter/http/Main.scala
+++ src/main/scala-2.11/org/romeo/loveletter/http/Main.scala
@@ -43,16 +43,39 @@ object Main extends App with SimpleRoutingApp {
 
   val teamToken = "K6kOMrLxkZfHoZvKIbE2Guzm"
 
+  val helpText = """
+    Rules are online at: http://www.alderac.com/tempest/files/2012/09/Love_Letter_Rules_Final.pdf
+    `/love help` to get this help
+    `/love start [player names]` to start a new game
+    `/love quit` to end the game in the channel
+    `/love hand` to see your hand
+    `/love status` to see all available information on the board
+    `/love play [card name] [?target] [?guess]` to play a card
+    Source for bot at: https://github.com/tylerjromeo/love-letter-slack-commands
+  """
+
+  def runCommand(text: String): SlackResponse = {
+    val params = text.split("\\s+")
+    params(0) match {
+      case "start" => SlackResponse(false, "start NOT YET IMPLEMENTED")
+      case "quit" => SlackResponse(false, "quit NOT YET IMPLEMENTED")
+      case "status" => SlackResponse(false, "status NOT YET IMPLEMENTED")
+      case "hand" => SlackResponse(false, "hand NOT YET IMPLEMENTED")
+      case "play" => SlackResponse(false, "play NOT YET IMPLEMENTED")
+      case _ => SlackResponse(true, helpText)
+    }
+  }
+
   startServer(interface = "localhost", port = Properties.envOrElse("PORT", "8080").toInt) {
     pathSingleSlash {
       post {
         formFields('token, 'team_id, 'team_domain, 'channel_id, 'channel_name, 'user_id, 'user_name, 'command, 'text, 'response_url) {
-          (token, teamId, teamDomain, channelId, channelName, userId, userName, command, test, responseUrl) =>
+          (token, teamId, teamDomain, channelId, channelName, userId, userName, command, text, responseUrl) =>
             validate(token == teamToken, "Request token does not match team") {
             respondWithMediaType(MediaTypes.`application/json`) {
-              complete(SlackResponse(false, "test hello")) //TODO: connect routes to game
+              complete(runCommand(text)) //TODO: connect routes to game
             }
-          }            
+          }
         }
       }
     }

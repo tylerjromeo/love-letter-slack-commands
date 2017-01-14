e353614adab5a1bdf6b672dc450b73e0c7e5f843
Tyler Romeo <tylerjromeo@gmail.com>
2017-01-14 09:25:03 -0500

    add help function

--- src/main/scala-2.11/org/romeo/loveletter/cli/Main/Main.scala
+++ src/main/scala-2.11/org/romeo/loveletter/cli/Main/Main.scala
@@ -18,8 +18,26 @@ object Main extends App {
   def processCommand(s: String): Unit = {
     s match {
       case s if s == "exit" => sys.exit(0)
-      case s => println(s)
+      case "help" => printHelp()
+      case _ => printHelp()
     }
+  }
+
+  def printHelp(): Unit = {
+    val help =
+      """
+        |Rules are online at: http://www.alderac.com/tempest/files/2012/09/Love_Letter_Rules_Final.pdf
+        |`help` to get this help
+        |`start [player names]` to start a new game
+        |`quit` to end the game
+        |`hand` to see your hand
+        |`status` to see all available information on the board
+        |`play [card name] [?target] [?guess]` to play a card. (play can be omitted)
+        |`discard` is equivalent to `play`
+        | prefix any command with a player name in parens to act as that player. If this is omitted the player whose turn it is will act
+        |Source for bot at: https://github.com/tylerjromeo/love-letter-slack-commands
+      """.stripMargin
+    println(help)
   }
 
 }

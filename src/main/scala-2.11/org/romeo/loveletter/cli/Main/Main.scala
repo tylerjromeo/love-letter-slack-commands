8538f0a2e2930f73ef1cd882b24436343be2033d
Tyler Romeo <tylerjromeo@gmail.com>
2017-01-14 09:59:28 -0500

    remove overridePlayer
    there's no reason to run a command as any player except the current player

--- src/main/scala-2.11/org/romeo/loveletter/cli/Main/Main.scala
+++ src/main/scala-2.11/org/romeo/loveletter/cli/Main/Main.scala
@@ -17,24 +17,16 @@ object Main extends App {
 
   def processCommand(s: String): Unit = {
     val split = s.split("""\s+""")
-    val (overridePlayer, rest) =
-      if(split(0).startsWith("(") && split(0).endsWith(")")) {
-        (Some(split(0)), split.tail.toList)
-      } else {
-        (None, split.toList)
-      }
-
-    rest match {
+    split.toList match {
       case command :: _ if command == "help" => printHelp()
       case command :: args if command == "start" => startGame(players = args)
       case command :: _ if command == "quit" => sys.exit(0)
-      case command :: _ if command == "hand" => showHand(overridePlayer = overridePlayer)
+      case command :: _ if command == "hand" => showHand()
       case command :: _ if command == "status" => showStatus()
       case command :: cardName :: args if command == "play" || command == "discard" => {
         val target = args.headOption
         val guess = if(args.size >= 2) Some(args(1)) else None
-        playCard(overridePlayer = overridePlayer,
-          card = cardName,
+        playCard(card = cardName,
           target = target,
           guess = guess)
       }
@@ -53,7 +45,6 @@ object Main extends App {
         |`status` to see all available information on the board
         |`play [card name] [?target] [?guess]` to play a card. (play can be omitted)
         |`discard` is equivalent to `play`
-        | prefix any command with a player name in parens to act as that player. If this is omitted the player whose turn it is will act
         |Source for bot at: https://github.com/tylerjromeo/love-letter-slack-commands
       """.stripMargin
     println(help)
@@ -61,10 +52,10 @@ object Main extends App {
 
   def startGame(players: List[String]): Unit = ???
 
-  def showHand(overridePlayer: Option[String]): Unit = ???
+  def showHand(): Unit = ???
 
   def showStatus(): Unit = ???
 
-  def playCard(overridePlayer: Option[String], card: String, target: Option[String], guess: Option[String]):Unit = ???
+  def playCard(card: String, target: Option[String], guess: Option[String]):Unit = ???
 
 }
